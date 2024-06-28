028b1d2df406ad3280cf8645b3630d17ae3625c2
Nitish Sadhu <nitishsadhu03@gmail.com>
2024-06-28 20:46:21 +0530

    initialised frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;

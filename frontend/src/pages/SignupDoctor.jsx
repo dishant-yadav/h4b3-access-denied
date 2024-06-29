66826016b33e810e631158e48c0d31b8eb94c705
Dishant Yadav <dishantyadav599@gmail.com>
2024-06-29 17:48:41 +0530

    fixed typos

--- frontend/src/pages/SignupDoctor.jsx
+++ frontend/src/pages/SignupDoctor.jsx
@@ -9,7 +9,19 @@ const SignUpDoctor = () => {
 
     const [userData, setUserData] = useState({
         email: "",
-        password: ""
+        password: "",
+        availability: [
+            {
+                "day": "Monday",
+                "startTime": "09:00",
+                "endTime": "17:00"
+            },
+            {
+                "day": "Wednesday",
+                "startTime": "09:00",
+                "endTime": "17:00"
+            }
+        ]
     });
 
     const handleSignUp = async (e) => {
@@ -59,7 +71,7 @@ const SignUpDoctor = () => {
                         }))}
                         className="w-full p-3 border border-gray-300 rounded-md mb-4"
                     />
-                    <Button type="submit" className="w-full p-3 bg-black text-white rounded-md">Sign In</Button>
+                    <Button type="submit" className="w-full p-3 bg-black text-white rounded-md">Sign Up</Button>
                 </form>
                 <p className="text-center text-gray-600 text-sm mt-6">
                     By clicking continue, you agree to our <a href="/terms" className="underline">Terms of Service</a> and <a href="/privacy" className="underline">Privacy Policy</a>.
